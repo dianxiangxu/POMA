d3025ac5ecfe2406cef739c5123ae79fac594377
vladi7 <dubrovenski.v@gmail.com>
2020-11-20 18:05:43 -0600

    Added the constraint for the previous access rights to BMC -Vlad

---
+++ src/main/java/POMA/Verification/BMC/ObligationChecker.java
@@ -0,0 +1,156 @@
+package POMA.Verification.BMC;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
+
+import gov.nist.csd.pm.pip.obligations.evr.EVRException;
+
+public class ObligationChecker extends BMC {
+
+	List<String> obligationsResponse = new ArrayList<String>();
+	List<String> obligationsEvents = new ArrayList<String>();
+
+	public ObligationChecker(int amount) throws Exception {
+		ObligationTranslator ot = new ObligationTranslator();
+		ot.processObligations();		
+		obligationsEvents.addAll(ot.getProcessedObligationsEvents());
+		obligationsResponse.addAll(ot.getProcessedObligations());
+	}
+
+	public ObligationChecker(Solver solver, int bound, int amount) {
+		super.setSolver(solver);
+		super.setBound(bound);
+
+	}
+
+	public String generateHeadCode() {
+		String smtlibv2Code = "";
+		smtlibv2Code += "(set-logic ALL_SUPPORTED)";
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(set-option :produce-models true)";
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(declare-fun Associations (Int) (Set (Tuple String String String)))";
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(declare-fun SetToCheckUA () (Set (Tuple String String)))";
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(declare-fun SetToCheckAT () (Set (Tuple String String)))";
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(assert (= SetToCheckUA (insert (mkTuple \"UA_test1\" \"UA_test1\") (mkTuple \"UA1_1\" \"UA1_1\") (mkTuple \"UA2_2\" \"UA2_2\") (singleton (mkTuple \"UA3_1_2\" \"UA3_1_2\")))))";
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(assert (= SetToCheckAT (insert (mkTuple \"Container1\" \"Container1\")  (singleton (mkTuple \"Container2\" \"Container2\")))))";
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(declare-fun Containment (Int) (Set (Tuple String String)))";
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(declare-fun Tclosure(Int) (Set (Tuple String String)))";
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(declare-fun AssociationsForUA (Int) (Set (Tuple String String String)))";
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(declare-fun UA_U_Reachability (Int) (Set (Tuple String String)))";
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(declare-fun AT_Reachability (Int) (Set (Tuple String String)))";
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(declare-fun AssignmentAdded (Int) (Set (Tuple String String)))";
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(declare-fun AssociationAdded (Int) (Set (Tuple String String String)))";
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(declare-fun FinalJoin(Int) (Set (Tuple String String String)))";
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(declare-fun SetToCheckUAOnlyAR () (Set (Tuple String String)))";
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(declare-fun FinalJoinOnlyAR (Int) (Set (Tuple String String String)))";
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(declare-fun SetToCheckATOnlyAR () (Set (Tuple String String)))";
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(assert (= SetToCheckUAOnlyAR (insert (mkTuple \"UA_test1\" \"0\") (mkTuple \"UA1_1\" \"0\") (mkTuple \"UA2_2\" \"0\") (singleton (mkTuple \"UA3_1_2\" \"0\")))))\n";
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(assert (= SetToCheckATOnlyAR (insert (mkTuple \"Container1\" \"0\")  (singleton (mkTuple \"Container2\" \"0\")))))";
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(assert (= (Tclosure "+0+") (tclosure (Containment "+0+"))))";
+		smtlibv2Code += System.lineSeparator();
+		//smtlibv2Code += "(assert (= (Associations "+0+") (insert (mkTuple \"UA_test1\" \"test1\" \"Container1\") (mkTuple \"UA_test1\" \"test3\" \"Container1\") (singleton (mkTuple \"UA_test1\" \"test2\" \"Container2\")))))";
+		smtlibv2Code += "(assert (= (Associations "+0+") (insert (mkTuple \"UA_test1\" \"test3\" \"Container1\") (singleton (mkTuple \"UA_test1\" \"test2\" \"Container2\")))))";
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(assert (= (Containment "+0+") (insert (mkTuple \"UA1_1\" \"UA1_1\") (mkTuple \"UA_test1\" \"UA_test1\")(mkTuple \"UA1_1\" \"Container1\") (mkTuple \"Container1\" \"Container1\") (mkTuple \"Container1\" \"PC1\") (mkTuple \"UA_test1\" \"PC1\")(mkTuple \"Container2\" \"Container2\") (mkTuple \"UA2_2\" \"Container2\") (mkTuple \"UA2_2\" \"UA2_2\") (mkTuple \"UA3_1_2\" \"Container2\") (mkTuple \"UA3_1_2\" \"UA3_1_2\") (mkTuple \"UA3_1_2\" \"Container1\") (mkTuple \"Container1\" \"PC1\") (singleton (mkTuple \"Container2\" \"PC1\")))))\n";
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(assert (= (UA_U_Reachability "+0+") (join SetToCheckUA (Tclosure "+0+"))))";
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(assert (= (AT_Reachability "+0+") (join SetToCheckAT (Tclosure "+0+"))))";
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(assert (= (AssociationsForUA "+0+") (join (UA_U_Reachability "+0+") (Associations "+0+")) ))";
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(assert (= (FinalJoin "+0+") (join (AssociationsForUA "+0+") (transpose (AT_Reachability "+0+"))) ))";
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(assert (= (FinalJoinOnlyAR "+0+") (join (transpose (join (FinalJoin "+0+")  SetToCheckATOnlyAR)) SetToCheckUAOnlyAR)))";
+		smtlibv2Code += System.lineSeparator();
+		return smtlibv2Code;
+	}
+
+	public String generateAssertKCode(int k, int randomNum) {		
+		String smtlibv2Code = System.lineSeparator();
+		smtlibv2Code +="(assert (member (mkTuple \"UA1_1\" \"test1\" \"Container1\") (FinalJoin "+k+")))";
+		smtlibv2Code +="(assert (member (mkTuple \"0\" \""+obligationsEvents.get(k)+"\" \"0\") (FinalJoinOnlyAR "+(k-1)+")))";
+		smtlibv2Code += System.lineSeparator();
+		return smtlibv2Code;
+	}
+
+	public String generateIterationCode(int k, int randomNum) {
+
+		String smtlibv2Code = "(assert (= (Tclosure "+k+") (tclosure (Containment "+k+"))))";
+
+		if(obligationsResponse.get(randomNum).contains("AssignmentAdded")) {
+
+			System.out.println(obligationsResponse.get(randomNum));
+			smtlibv2Code += obligationsResponse.get(randomNum);
+			smtlibv2Code += System.lineSeparator();
+			smtlibv2Code += "(assert (= (Containment "+k+") (union (AssignmentAdded "+ k +") (insert (mkTuple \"UA1_1\" \"UA1_1\") (mkTuple \"UA_test1\" \"UA_test1\")(mkTuple \"UA1_1\" \"Container1\") (mkTuple \"Container1\" \"Container1\") (mkTuple \"Container1\" \"PC1\") (mkTuple \"UA_test1\" \"PC1\")(mkTuple \"Container2\" \"Container2\") (mkTuple \"UA2_2\" \"Container2\") (mkTuple \"UA2_2\" \"UA2_2\") (mkTuple \"UA3_1_2\" \"Container2\") (mkTuple \"UA3_1_2\" \"UA3_1_2\") (mkTuple \"UA3_1_2\" \"Container1\") (mkTuple \"Container1\" \"PC1\") (singleton (mkTuple \"Container2\" \"PC1\"))))))\n";
+			smtlibv2Code += System.lineSeparator();
+		}
+		
+		else {
+			smtlibv2Code += System.lineSeparator();
+			smtlibv2Code += "(assert (= (Containment "+k+") (insert (mkTuple \"UA1_1\" \"UA1_1\") (mkTuple \"UA_test1\" \"UA_test1\")(mkTuple \"UA1_1\" \"Container1\") (mkTuple \"Container1\" \"Container1\") (mkTuple \"Container1\" \"PC1\") (mkTuple \"UA_test1\" \"PC1\")(mkTuple \"Container2\" \"Container2\") (mkTuple \"UA2_2\" \"Container2\") (mkTuple \"UA2_2\" \"UA2_2\") (mkTuple \"UA3_1_2\" \"Container2\") (mkTuple \"UA3_1_2\" \"UA3_1_2\") (mkTuple \"UA3_1_2\" \"Container1\") (mkTuple \"Container1\" \"PC1\") (singleton (mkTuple \"Container2\" \"PC1\")))))\n";
+		}	
+		if(obligationsResponse.get(randomNum).contains("AssociationAdded")) {
+			System.out.println(obligationsResponse.get(randomNum));
+			smtlibv2Code += obligationsResponse.get(randomNum);
+			smtlibv2Code += System.lineSeparator();
+			smtlibv2Code += "(assert (= (Associations "+k+") (union (AssociationAdded "+ k +") (insert (mkTuple \"UA_test1\" \"test1\" \"Container1\") (mkTuple \"UA_test1\" \"test3\" \"Container1\") (singleton (mkTuple \"UA_test1\" \"test2\" \"Container2\"))))))";
+			smtlibv2Code += System.lineSeparator();
+		}
+		else {
+			smtlibv2Code += System.lineSeparator();
+			//smtlibv2Code += "(assert (= (Associations "+k+") (insert (mkTuple \"UA_test1\" \"test1\" \"Container1\") (mkTuple \"UA_test1\" \"test3\" \"Container1\") (singleton (mkTuple \"UA_test1\" \"test2\" \"Container2\")))))";
+			smtlibv2Code += "(assert (= (Associations "+k+") (insert (mkTuple \"UA_test1\" \"test3\" \"Container1\") (singleton (mkTuple \"UA_test1\" \"test2\" \"Container2\")))))";
+		}
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(assert (= (UA_U_Reachability "+k+") (join SetToCheckUA (Tclosure "+k+"))))";
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(assert (= (AT_Reachability "+k+") (join SetToCheckAT (Tclosure "+k+"))))";
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(assert (= (AssociationsForUA "+k+") (join (UA_U_Reachability "+k+") (Associations "+k+")) ))";
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(assert (= (FinalJoin "+k+") (join (AssociationsForUA "+k+") (transpose (AT_Reachability "+k+"))) ))";
+		smtlibv2Code += System.lineSeparator();
+		smtlibv2Code += "(assert (= (FinalJoinOnlyAR "+k+") (join (transpose (join (FinalJoin "+k+")  SetToCheckATOnlyAR)) SetToCheckUAOnlyAR)))";
+		smtlibv2Code += System.lineSeparator();
+		return smtlibv2Code;
+	}
+
+	
+	public static void main(String[] args) throws Exception {
+		ObligationChecker checker = new ObligationChecker(115);
+		checker.setSMTCodePath("SMTLIBv2Files/SMTLIB2Input/BMCFiles/BMC");
+		checker.check();
+	}
+
+}
---
+++ src/main/java/POMA/Verification/BMC/ObligationTranslator.java
@@ -0,0 +1,140 @@
+package POMA.Verification.BMC;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+import org.apache.commons.io.IOUtils;
+
+import gov.nist.csd.pm.pip.obligations.Obligations;
+import gov.nist.csd.pm.pip.obligations.evr.EVRException;
+import gov.nist.csd.pm.pip.obligations.evr.EVRParser;
+import gov.nist.csd.pm.pip.obligations.model.Obligation;
+import gov.nist.csd.pm.pip.obligations.model.Rule;
+import gov.nist.csd.pm.pip.obligations.model.actions.Action;
+import gov.nist.csd.pm.pip.obligations.model.actions.AssignAction;
+import gov.nist.csd.pm.pip.obligations.model.actions.AssignAction.Assignment;
+import gov.nist.csd.pm.pip.obligations.model.actions.GrantAction;
+
+public class ObligationTranslator {
+	int k  = 1;
+
+	String pathToObligations = "POMA/Verification/BMC/obligation.yml";
+	List<String> processedObligations = new ArrayList<String>();
+	List<String> processedObligationsEvents = new ArrayList<String>();
+
+	public List<String> getProcessedObligations() {
+		return processedObligations;
+	}
+	public List<String> getProcessedObligationsEvents() {
+		return processedObligationsEvents;
+	}
+	public static void main (String[] args) throws Exception {
+		ObligationTranslator ot = new ObligationTranslator();
+		ot.processObligations();
+	}
+	
+	public String processObligations() throws EVRException, IOException {
+		Obligation obligation = readObligations();
+		translateObligation(obligation);
+		return null;
+	}
+
+	private void translateObligation(Obligation obligation) {
+		processedObligations.add("");
+		processedObligationsEvents.add("");
+
+		for(Rule r : obligation.getRules()) {
+			System.out.println(getEvents(r));
+			processedObligationsEvents.addAll(getEvents(r));
+			processActions(r);
+		}
+	}
+	
+	private Obligation readObligations() throws EVRException, IOException {
+		InputStream is = getClass().getClassLoader().getResourceAsStream(pathToObligations);
+		String yml = IOUtils.toString(is, StandardCharsets.UTF_8.name());
+		Obligation obligation = EVRParser.parse(yml);
+		return obligation;
+	}
+	
+	private List<String> getEvents(Rule rule) {
+		return rule.getEventPattern().getOperations();
+	}
+	
+	private void processActions(Rule rule) {
+		List<AssignAction> assignActions = new ArrayList<AssignAction>();
+		List<GrantAction> grantActions = new ArrayList<GrantAction>();
+
+		for(Action action : rule.getResponsePattern().getActions())
+		{
+			if(action instanceof AssignAction) {
+				assignActions.add((AssignAction)action);
+			}
+			else if(action instanceof GrantAction) {
+				System.out.println("HELLO");
+				grantActions.add((GrantAction)action);
+			}
+		}	
+		//System.out.println(processGrantAction(grantActions));
+		processedObligations.add(processAssignAction(assignActions) + System.lineSeparator() + processGrantAction(grantActions));
+		k++;
+	}
+	
+	private String processAssignAction(List<AssignAction> actions) {
+		if(actions.size() == 0) return ""; 
+		if(actions.size() == 1) {
+			String what = actions.get(0).getAssignments().get(0).getWhat().getName().toString();
+			String where = actions.get(0).getAssignments().get(0).getWhere().getName().toString();
+			return "(assert (= (AssignmentAdded "+k+") (singleton(mkTuple \""+what+"\" \""+where+"\"))))";
+		}
+		ListIterator<AssignAction> iterator =actions.listIterator();		
+		StringBuilder sb = new StringBuilder();
+		sb.append("\r\n");
+		sb.append("(assert (= (AssignmentAdded "+k+") (insert ");
+		while(iterator.hasNext()) {
+			Assignment assignment = iterator.next().getAssignments().get(0);
+			String what = assignment.getWhat().getName().toString();
+			String where = assignment.getWhere().getName().toString();
+			if(!iterator.hasNext()) {
+				sb.append("(singleton(mkTuple \""+what+"\" \""+where+"\")))))");
+				break;
+			}
+			sb.append(" (mkTuple \""+what+"\" \""+where+"\") ");		
+			
+		}
+		return sb.toString();		
+	}
+
+	private String processGrantAction(List<GrantAction> actions) {
+		if(actions.size() == 0) return ""; 
+		if(actions.size() == 1) {
+			String what = actions.get(0).getSubject().getName().toString();
+			String where = actions.get(0).getTarget().getName().toString();
+			String op = actions.get(0).getOperations().get(0);
+
+			return "(assert (= (AssociationAdded "+k+") (singleton(mkTuple \""+what+"\" \""+ op +"\" \""+where+"\"))))";
+		}
+		ListIterator<GrantAction> iterator =actions.listIterator();		
+		StringBuilder sb = new StringBuilder();
+		sb.append("\r\n");
+		sb.append("(assert (= (AssociationAdded "+k+") (insert ");
+		while(iterator.hasNext()) {
+			GrantAction association = iterator.next();
+			String what = association.getSubject().getName().toString();
+			String where = association.getTarget().getName().toString();
+			String op = association.getOperations().get(0);
+
+			if(!iterator.hasNext()) {
+				sb.append("(singleton(mkTuple \""+what+"\" \""+ op +"\" \""+where+"\")))))");
+				break;
+			}
+			sb.append(" (mkTuple \""+what+"\" \""+op+"\" \""+where+"\") ");		
+			
+		}
+		return sb.toString();		
+	}	
+}
